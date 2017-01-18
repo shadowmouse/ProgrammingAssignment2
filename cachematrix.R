2d57c5db32c302a931b23e5cb12a6250b8a58cc9
Elliot Francis <signup@windowlatch.org>
2017-01-18 11:56:25 -0700

    Assignment Implementation

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Author : Elliot Francis
+## Date Created : 2017-1-18
+## Date Modified : 2017-1-18
+## Description : Assignment 2 - Demonstrate Lexical Scoping via a Cached Matrix Custom Object
+##               Object will manage access to matrix data, returning a cached object on unchanged matrix.
 
-## Write a short comment describing this function
+## Two storage objects for the matrix and a caching mechanism
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(matrix = matrix()) {
+  matrixInverse <- NULL
+  set <- function (m) {
+    matrix <<- m
+    matrixInverse <<- NULL
+  }
+  get <- function () { matrix }
+  setInverse <- function (inverse) { matrixInverse <<- inverse }
+  getInverse <- function() { matrixInverse }
+  list(
+    set = set,
+    get = get,
+    setInverse = setInverse,
+    getInverse = getInverse
+  )
 }
 
-
-## Write a short comment describing this function
+## Solve a matrix, obtaining it's inverse. Returns a Cached Value if the inverse was already calculated
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inverse <- x$getInverse()
+    if(!is.null(inverse)) {
+      print("Returning Cached Inverse")
+      return(inverse)
+    }
+    matrix <- x$get();
+    inverse <- solve(matrix);
+    x$setInverse(inverse)
+    inverse
 }
